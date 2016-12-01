e2452382e54e5c13582ccc32f14a2dcb0e189837
Laucr <leo.lau.inami@gmail.com>
2016-12-01 17:25:38 +0800

    delete test.py

--- Word_Segmentation/test.py
+++
@@ -1,7 +0,0 @@
-import ws
-import Text_Filter.tf as tf
-import os
-
-set_dict = tf.TextFilter.get_sets_of_root_path_tree(os.path.abspath('../ntest/sort1/train'))
-w = ws.WordSegmentation(set_dict)
-w.wd_seg()
